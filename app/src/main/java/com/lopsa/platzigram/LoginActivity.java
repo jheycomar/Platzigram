74689f270b3e7547aac2f95193fb601678a5822c
Omar <jheycovich@gmail.com>
2017-11-20 10:55:29 -0400

    add login and register new user

--- app/src/main/java/com/lopsa/platzigram/LoginActivity.java
+++ app/src/main/java/com/lopsa/platzigram/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.lopsa.platzigram;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -16,8 +23,12 @@ import butterknife.OnClick;
 public class LoginActivity extends  android.support.v7.app.AppCompatActivity  {
 
     private Button btnlogin;
-    //@BindView(R.id.lblcreateaccount) TextView lblcreateaccound;
-//gracias ala libreria Butter Knife "cuchillo de mantequilla"
+    @BindView(R.id.txtuser)
+    TextInputEditText etemail;
+    @BindView(R.id.txtpassword) TextInputEditText etpassword;
+    //gracias ala libreria Butter Knife "cuchillo de mantequilla"
+    private FirebaseAuth firebaseAuth;
+
     @OnClick(R.id.lblcreateaccount) void registrar() {
         Toast.makeText(this, "entro", Toast.LENGTH_SHORT).show();
         Intent intent=new Intent(LoginActivity.this,createaccountActivity.class);
@@ -31,20 +42,37 @@ public class LoginActivity extends  android.support.v7.app.AppCompatActivity  {
 
         ButterKnife.bind(this);
         btnlogin=(Button)findViewById(R.id.btnlogin);
+        firebaseAuth= FirebaseAuth.getInstance();
        // lblcreateaccound=(TextView)findViewById(R.id.lblcreateaccount);
 
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                   //los flags son para borrar el historial de navegacion
-                    Intent intent = new Intent( LoginActivity.this, homeActivity.class );
-                    startActivity(intent);
-                }
-                catch (Exception ex){
-                    Toast.makeText(LoginActivity.this, ""+ex.toString(), Toast.LENGTH_SHORT).show();
-                }
+                String email=etemail.getText().toString().trim();
+                String password=etpassword.getText().toString().trim();
+              if (email.equals("")){
+                  Toast.makeText(LoginActivity.this, "Proporcione un Nombre de Usuario valido", Toast.LENGTH_SHORT).show();
+                  return;
+              }
+              if (password.equals("")){
+                  Toast.makeText(LoginActivity.this, "Proporcione una contraseña valida", Toast.LENGTH_SHORT).show();
+                  return;
+              }
+
+              firebaseAuth.signInWithEmailAndPassword(email,password)
+                      .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                          @Override
+                          public void onComplete(@NonNull Task<AuthResult> task) {
+                             if (!task.isSuccessful()){
+                                 Toast.makeText(LoginActivity.this, "Login Failed"+task.getException(), Toast.LENGTH_SHORT).show();
+                             }
+                             else {
+                             startActivity(new Intent(LoginActivity.this,homeActivity.class));
+                             }
+                          }
+              });
+
             }
         });
     }
--- app/src/main/java/com/lopsa/platzigram/createaccountActivity.java
+++ app/src/main/java/com/lopsa/platzigram/createaccountActivity.java
@@ -1,20 +1,92 @@
 package com.lopsa.platzigram;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnLongClick;
 
 public class createaccountActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtcorreo)   TextInputEditText txtCorreo;
+    @BindView(R.id.txtnombre)   TextInputEditText txtNombre;
+    @BindView(R.id.txtusuario)   TextInputEditText txtUser;
+    @BindView(R.id.txtcontrasena)   TextInputEditText txtContrasena;
+    @BindView(R.id.txtconfircontrasena)   TextInputEditText txtConfirContrasena;
+    @BindView(R.id.btncreateaccount)   Button btnCreateAcccount;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createaccount);
+        ButterKnife.bind(this);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
 
       //  Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
        // setSupportActionBar(toolbar);
 
         showToolbar(getResources().getString(R.string.toolbar_title),true);
+
+
+    }
+
+    @OnClick(R.id.btncreateaccount) void crearCuenta(){
+
+        String email=txtCorreo.getText().toString().trim();
+        String name=txtNombre.getText().toString().trim();
+        String userName=txtUser.getText().toString().trim();
+        String password=txtContrasena.getText().toString().trim();
+        String passwordconfirmation=txtConfirContrasena.getText().toString().trim();
+        if (email.equals("")){
+            Toast.makeText(this, " porfavor Ingrese Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(passwordconfirmation)){
+            if (password.equals("")){
+                Toast.makeText(this, "Passwor Invalid", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        Toast.makeText(this, "el password es igual", Toast.LENGTH_SHORT).show();
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(createaccountActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    Toast.makeText(createaccountActivity.this, "Auth failed"+task.getException(), Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    startActivity(new Intent(createaccountActivity.this,LoginActivity.class));
+                    finish();
+
+                }
+            }
+        });
+
     }
 
     //metodo que activa el boton atras
